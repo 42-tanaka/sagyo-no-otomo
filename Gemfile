644916b1dc3d3b7b5e992996730316228b73b923
TanakaIkuho <sonrisa.enero@gmail.com>
2024-01-23 03:00:54 +0900

    add:lineログイン

--- Gemfile
+++ Gemfile
@@ -76,3 +76,8 @@ group :test do
 end
 
 gem 'devise'
+
+gem 'omniauth-line'
+gem 'omniauth-rails_csrf_protection'
+
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  
-  devise_for :users
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root "posts#index"
 end

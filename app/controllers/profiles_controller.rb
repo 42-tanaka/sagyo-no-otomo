f8e64f4d3d3ea52266841bc533cb1193e0fe1082
TanakaIkuho <sonrisa.enero@gmail.com>
2024-01-25 15:52:34 +0900

    ユーザー登録・変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,37 @@
 class ProfilesController < ApplicationController
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to root_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
   end
 
   def edit
+    @profile = current_user.profile
   end
 
   def update
+    @profile = current_user.profile
+    if @profile.update(profile_params)
+      redirect_to profile_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit
+    end
   end
 
-  def show
+  def show; end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :avatar)
   end
 end

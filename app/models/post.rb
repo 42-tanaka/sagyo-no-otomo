17adabe5e93fec08bb5e6729dfd8b9ab45cbec40
TanakaIkuho <sonrisa.enero@gmail.com>
2024-05-01 02:23:00 +0900

    posts_controllerのSearchメソッド修正#38

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,13 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 255 }
 
   enum category: { food: 0, drink: 1 }
+
+  scope :eaten, -> { where(eaten: true) }
+  scope :smelly, -> { where(smell: true) }
+  scope :noisy, -> { where(sound: true) }
+  scope :spilly, -> { where(spill: true) }
+  scope :dirty_hands, -> { where(hands_dirty: true) }
+  scope :by_category, ->(category) { where(category: category) if category.present? }
+  scope :recent, -> { order(created_at: :desc) }
+
 end

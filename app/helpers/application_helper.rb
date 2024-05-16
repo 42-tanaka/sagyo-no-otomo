416a952fbc20b940854f2391405048e34535feac
TanakaIkuho <sonrisa.enero@gmail.com>
2024-05-16 15:41:18 +0900

    ogp設定修正_2#78

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,52 +1,30 @@
 module ApplicationHelper
 
-  def show_meta_tags
-    assign_meta_tags if display_meta_tags.blank?
-    display_meta_tags
-  end
-
-  def assign_meta_tags(options = {})
-    defaults = {
+  def default_meta_tags
+    {
       site: '作業のオトモ',
       title: 'デスクワーク作業のお供に食べたお菓子や飲み物を記録・シェアするサービス',
-      description: 'デスクワーク作業のお供に食べたお菓子や飲み物を、職場だと気になってしまうポイントと一緒に記録・シェアしましょう。',
-      keywords: 'デスクワーク, 作業, お供, お菓子, 飲み物'
-    }
-    options.reverse_merge!(defaults)
-    site = options[:site]
-    title = options[:title]
-    description = options[:description]
-    keywords = options[:keywords]
-    image = options[:image].presence || image_url('ogp.png')
-
-    configs = {
-      separator: '|',
       reverse: true,
-      site: site,
-      title: title,
-      description: description,
-      keywords: keywords,
+      separator: '|',
+      description: 'デスクワーク作業のお供に食べたお菓子や飲み物を、職場だと気になってしまうポイントと一緒に記録・シェアしましょう。',
+      keywords: 'デスクワーク, 作業, お供, お菓子, 飲み物',
       canonical: request.original_url,
-      icon: {
-        href: image_url('ogp.png')
-      },
+      image: :image.presence || image_url('logo.png'),
+
       og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
         type: 'website',
-        title: title.presence || site,
-        description: description,
         url: request.original_url,
-        image: ,
-        site_name: site
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
       },
       twitter: {
-        title: ,
-        site: ,
         card: 'summary_large_image',
-        image:
+        site: '@',
       }
     }
-
-    set_meta_tags(configs)
   end
 
 end
